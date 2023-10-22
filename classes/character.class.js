0bb2ae481c4d32f562913e28bd7213334911d4e7
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-10-22 22:01:46 +0200

    animations
    added first simple animations for character and chicken

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,13 +1,36 @@
 class Character extends MoveableObject {
-    y = 243
-    width = 610 / 5
-    height = 1200 / 5
+    y = 243;
+    width = 610 / 5;
+    height = 1200 / 5;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
+
+
     jump() {
         
     }
 
     constructor() {
-        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000 / 11)
     }
 
 }
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,11 +2,26 @@ class Chicken extends MoveableObject {
     y = 425
     width = 248 / 5
     height = 248 / 5
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    ];
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
-
+        this.loadImages(this.IMAGES_WALKING);
         this.x = 250 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000 / 5)
     }
 
 }
