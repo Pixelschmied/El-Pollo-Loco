f95c5e061caa27f141e499ad4fc35caef5a502bd
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-10-24 23:42:54 +0200

    character movement shortcuts

--- classes/character.class.js
+++ classes/character.class.js
@@ -11,6 +11,7 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    speed = 4;
 
 
     jump() {
@@ -27,13 +28,22 @@ class Character extends MoveableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+            
+        }, 1000 / 60)
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-            
-        }, 1000 / 11)
+        }, 75)
     }
 
 }
