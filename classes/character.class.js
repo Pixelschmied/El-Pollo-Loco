07080834d47520d69bc1aed625e038a67f7f88dc
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-10-24 23:28:17 +0200

    added keyboard functions to character

--- classes/character.class.js
+++ classes/character.class.js
@@ -10,6 +10,7 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    world;
 
 
     jump() {
@@ -25,10 +26,13 @@ class Character extends MoveableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+            
         }, 1000 / 11)
     }
 
