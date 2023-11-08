a637ccbf29515056a790574c21dc8c52a22aff80
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2023-11-08 19:28:00 +0100

    files cleanup

--- js/classes/drawable-object.class.js
+++ js/classes/drawable-object.class.js
@@ -26,7 +26,7 @@ class DrawableObject {
     }
 
     drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
+        if (this instanceof Character || this instanceof Chicken || this instanceof Endboss || this instanceof ThrowableObject) {
         ctx.beginPath();
         ctx.lineWidth = '2';
         ctx.strokeStyle = 'blue'
