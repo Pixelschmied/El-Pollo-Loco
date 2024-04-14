75ba3d7afd3868ee283e705d53c29d6a30835ba2
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-14 23:52:48 +0200

    Update bottle-counter.class.js

--- js/classes/bottle-counter.class.js
+++ js/classes/bottle-counter.class.js
@@ -1,15 +1,16 @@
 class BottleCounter extends DrawableObject {
 
-    bottleIcon = "img/6_salsa_bottle/salsa_bottle.png";
-    bottleCount = 0;
+    bottleIcon = 'img/6_salsa_bottle/salsa_bottle.png';
+    textX = 75;
+    textY = 96;
 
     constructor() {
-        super();
+        super().loadImage(this.bottleIcon);
         this.loadImages(this.bottleIcon);
         this.width = 400 / 7;
         this.height = 400 / 7;
         this.x = 30;
-        this.y = 50;
+        this.y = 54;
         this.img = this.imageCache[this.bottleIcon];
     }
 
@@ -17,6 +18,20 @@ class BottleCounter extends DrawableObject {
             let img = new Image();
             img.src = path;
             this.imageCache[path] = img;
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        ctx.font = '40px boogaloo';
+        const gradient = ctx.createLinearGradient(76, 70, 108, 95);
+        gradient.addColorStop(0.15, 'yellow');
+        gradient.addColorStop(0.75, 'orange');
+        gradient.addColorStop(1.0, 'yellow');
+        ctx.fillStyle = gradient;
+        ctx.strokeStyle = 'black';
+        ctx.fillText(this.bottleCount, this.textX, this.textY);
+        ctx.strokeText(this.bottleCount, this.textX, this.textY);
     }
 
 }
