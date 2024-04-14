7bf240dde3b74401393baa7adf5a727f6c8d3560
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-14 23:53:15 +0200

    Update coin-counter.class.js

--- js/classes/coin-counter.class.js
+++ js/classes/coin-counter.class.js
@@ -1,20 +1,36 @@
 class CoinCounter extends DrawableObject {
-    bottleIcon = "img/8_coin/coin_2.png";
-    bottleCount = 0;
+    coinIcon = 'img/8_coin/coin_2.png';
+    textX = 178;
+    textY = 96;
 
     constructor() {
         super();
-        this.loadImages(this.bottleIcon);
+        this.loadImages(this.coinIcon);
         this.width = 300 / 2.2;
         this.height = 301 / 2.2;
         this.x = 80;
-        this.y = 13;
-        this.img = this.imageCache[this.bottleIcon];
+        this.y = 15;
+        this.img = this.imageCache[this.coinIcon];
     }
 
     loadImages(path) {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = img;
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        ctx.font = '40px boogaloo';
+        const gradient = ctx.createLinearGradient(185, 70, 218, 95);
+        gradient.addColorStop(0.15, 'yellow');
+        gradient.addColorStop(0.75, 'orange');
+        gradient.addColorStop(1.0, 'yellow');
+        ctx.fillStyle = gradient;
+        ctx.strokeStyle = 'black';
+        ctx.fillText(this.coinCount, this.textX, this.textY);
+        ctx.strokeText(this.coinCount, this.textX, this.textY);
+
     }
 }
