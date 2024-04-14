e8a9c02ce8b5a81aa7d1e5edaa823dc1eaeab970
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-14 23:56:15 +0200

    Update throwable-object.class.js

--- js/classes/throwable-object.class.js
+++ js/classes/throwable-object.class.js
@@ -1,24 +1,24 @@
 class ThrowableObject extends MoveableObject {
 
     IMAGES_THROWN = [
-        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png"
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
     ];
     IMAGES_SPLASH = [
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
-        "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png"
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
     ]
     bottleBroken = false;
     breakStartTime = 0;
 
     constructor(character, x, y, mirrored, throwableObjects) {
-        super().loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+        super().loadImage(this.IMAGES_THROWN[0]);
         this.loadImages(this.IMAGES_THROWN);
         this.loadImages(this.IMAGES_SPLASH);
         this.character = character;
@@ -108,7 +108,7 @@ class ThrowableObject extends MoveableObject {
             this.bottleBroken = true;
             this.breakBottle();
             this.upForce = 0;
-            //console.log("Bottle removed")
+            //console.log('Bottle removed')
             //this.throwableObjects.pop();
         }
     }
