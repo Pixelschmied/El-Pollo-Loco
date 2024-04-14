c1e6df01104d4fe2a87a330b623e9ab4b65808a3
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-14 23:54:00 +0200

    Update level.class.js

--- js/classes/level.class.js
+++ js/classes/level.class.js
@@ -1,12 +1,26 @@
 class Level {
-    enemies;
-    clouds;
-    backgroundObjects;
-    level_end_x = 2300;
+    enemies = [];
+    clouds = [];
+    bottles = [];
+    backgroundObjects = [];
+    level_end_x = 4200;
 
-    constructor(enemies, clouds, backgroundObjects){
-        this.enemies = enemies;
-        this.clouds = clouds;
+    constructor(chicken, chicks, bottles, clouds, backgroundObjects) {
+        for (let i = 0; i < chicken; i++) {
+            this.enemies.push(new Chicken());
+        }
+        for (let i = 0; i < chicks; i++) {
+            this.enemies.push(new Chick());
+        }
+        this.enemies.push(new Endboss());
+        for (let i = 0; i < bottles; i++) {
+            this.bottles.push(new Bottle());
+        }
+        for (let i = 0; i < clouds; i++) {
+            this.clouds.push(new Cloud());
+        }
         this.backgroundObjects = backgroundObjects;
     }
+
+
 }
