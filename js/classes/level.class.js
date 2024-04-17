22419c1fa8835e1f62f40c377dc64ee824870b1a
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-18 01:25:35 +0200

    added coins to level creation

--- js/classes/level.class.js
+++ js/classes/level.class.js
@@ -2,10 +2,11 @@ class Level {
     enemies = [];
     clouds = [];
     bottles = [];
+    coins = [];
     backgroundObjects = [];
     level_end_x = 4200;
 
-    constructor(chicken, chicks, bottles, clouds, backgroundObjects) {
+    constructor(chicken, chicks, bottles, coins, clouds, backgroundObjects) {
         for (let i = 0; i < chicken; i++) {
             this.enemies.push(new Chicken());
         }
@@ -15,6 +16,9 @@ class Level {
         this.enemies.push(new Endboss());
         for (let i = 0; i < bottles; i++) {
             this.bottles.push(new Bottle());
+        }
+        for (let i = 0; i < coins; i++) {
+            this.coins.push(new Coin());
         }
         for (let i = 0; i < clouds; i++) {
             this.clouds.push(new Cloud());
