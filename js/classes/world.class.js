9b7deabd6b514812a65e39860a9f9862821b581e
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-18 01:28:17 +0200

    added: endboss healthbar, coin & bottle collision

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -5,7 +5,8 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
-    statusBar = new StatusBar();
+    characterHealthBar = new CharacterHealthBar()
+    endbossHealthBar = new EndbossHealthBar();
     alertBorder = new AlertBorder();
     bottleCount = new BottleCounter();
     bottleCountDown = Date.now();
@@ -33,22 +34,22 @@ class World {
     }
 
     checkThrowableObjects() {
-        if (this.keyboard.E && this.throwableObjects.length < 1 && Date.now() - this.bottleCountDown > 1500) {
+        if (this.keyboard.E && DrawableObject.bottleCount > 0 && Date.now() - this.bottleCountDown > 1500) {
             let mirrored = this.character.mirrored;
             this.bottleCountDown = Date.now();
             this.character.resetIdleStartTime();
             let bottle = new ThrowableObject(this.character, this.character.x + (this.character.width / 2), this.character.y + (this.character.height / 2), mirrored, this.throwableObjects)
             this.throwableObjects.push(bottle);
+            DrawableObject.bottleCount--;
         }
     }
 
     checkCollisions() {
         // Character Collision with Enemies
         this.level.enemies.forEach((enemy) => {
-            if ((this.character.isColliding(enemy) && !this.character.isFalling() && enemy.died == false && this.character.possibleHeadJump == false) || 
+            if ((this.character.isColliding(enemy) && !this.character.isFalling() && enemy.died == false && this.character.possibleHeadJump == false) ||
                 (this.character.isColliding(enemy) && enemy instanceof Endboss)) {
-                    this.character.hit();
-                    this.statusBar.setPercentage(this.character.life);
+                this.character.hit();
             }
         });
         // Character Headjump Collision with Enemies
@@ -66,31 +67,53 @@ class World {
                     throwableObject.bottleBroken = true;
                     enemy.died = true;
                 }
+                if (throwableObject.isColliding(enemy) && enemy instanceof Endboss) {
+                    throwableObject.bottleBroken = true;
+                    enemy.endbossHit();
+                    console.log(Endboss.life);
+                }
             });
+        });
+        // Character Collision with Bottles
+        this.level.bottles.forEach((bottle, index) => {
+            if (this.character.isColliding(bottle)) {
+                DrawableObject.bottleCount++;
+                this.level.bottles.splice(index, 1);
+            }
+        });
+        // Character Collision with Coins
+        this.level.coins.forEach((coin, index) => {
+            if (this.character.isColliding(coin)) {
+                DrawableObject.coinCount++;
+                this.level.coins.splice(index, 1);
+            }
         });
     }
 
     draw() {
-        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
         // Dynamic Objects
         this.addToMap(this.level.backgroundObjects);
         this.addToMap(this.level.clouds);
         this.addToMap(this.level.enemies);
+        this.addToMap(this.level.bottles);
         this.addToMap(this.throwableObjects);
+        this.addToMap(this.level.coins);
         this.drawToMap(this.character);
         this.ctx.translate(-this.camera_x, 0);
 
         // Static Objects
         this.drawToMap(this.alertBorder);
-        this.drawToMap(this.statusBar);
+        this.drawToMap(this.characterHealthBar);
+        this.drawToMap(this.endbossHealthBar);
         this.drawToMap(this.bottleCount);
         this.drawToMap(this.coinCounter);
         let self = this;
-        requestAnimationFrame(function() {self.draw();});
+        requestAnimationFrame(function () { self.draw(); });
     }
-    
+
 
     addToMap(objects) {
         objects.forEach(o => {
@@ -104,7 +127,7 @@ class World {
         }
 
         mo.draw(this.ctx)
-        mo.drawFrame(this.ctx) // TODO: Frame Function (delete if not needed)
+        //mo.drawFrame(this.ctx) // TODO: Frame Function (delete if not needed)
         //mo.debugText(this.ctx) // TODO: Debug Text Function (delete if not needed)
         //mo.drawXDot(this.ctx) // TODO: Dot Function (delete if not needed)
 
