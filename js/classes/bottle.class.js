77b2c71dac1f8b7c9982576759554d651ca65ad9
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-17 13:47:26 +0200

    Update bottle.class.js

--- js/classes/bottle.class.js
+++ js/classes/bottle.class.js
@@ -1,5 +1,5 @@
 class Bottle extends DrawableObject {
-    y = 150;
+    y = 300;
     bottleIcons = [
         'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
         'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
@@ -9,13 +9,16 @@ class Bottle extends DrawableObject {
         //super().loadImage(this.bottleIcons[Math.round(Math.random())]);
         super().loadImage(this.bottleIcons[0]);
         this.loadImages(this.bottleIcons);
-        this.x = 250// + Math.random() * 3800;
-        this.width = 400 / 7;
-        this.height = 400 / 7;
-        this.img = this.bottleIcons[Math.round(Math.random())]
+        this.x = 250 + Math.round(Math.random() * 500);
+        this.width = 57;
+        this.height = 57;
+        //this.img = this.bottleIcons[Math.round(Math.random())]
+        console.log("Bottle created: ")
+        console.log("X: ", this.x, ", Y: ", this.y, "Width: ", this.width, "Height: ", this.height)
+        console.log("Image: ", this.img)
     }
 
-    //draw(ctx) {
-    //    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    //}
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
 }
