0043b9a7f82a6a2ff14265401523e959bce89779
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-04-18 01:20:36 +0200

    Create character-health-bar.class.js

---
+++ js/classes/character-health-bar.class.js
@@ -0,0 +1,29 @@
+class CharacterHealthBar extends DrawableObject {
+
+    IMAGES = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
+    ];
+
+    constructor() {
+        super();
+        this.loadImages(this.IMAGES);
+        this.width = 595 / 3;
+        this.height = 158 / 3;
+        this.x = 30;
+        this.y = 0;
+        this.img = this.imageCache[this.IMAGES[Character.life]]
+    }
+
+    //setPercentage() {
+    //    let path = this.IMAGES[Character.life];
+    //    this.x = 30;
+    //    this.y = 0;
+    //    this.img = this.imageCache[path]
+    //    console.log(Character.life)
+    //}
+}
